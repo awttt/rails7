12fe00eb980586904e1b235edceedc45cfcf6335
awt <350944364@qq.com>
2022-08-29 14:22:26 +0800

    测试重复发邮件

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,6 @@
 class UserMailer < ApplicationMailer
   def welcome_email(email)
+    #.order(create_at: :desc) 找最后生成的数据
     validation_code = ValidationCode.order(create_at: :desc).find_by_email(email)
     @code = validation_code.code
     mail(to: email, subject: '验证码')
