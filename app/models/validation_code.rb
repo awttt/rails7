d248f66dfac6195df03d11a0166121e0fbc9f7a0
awt <350944364@qq.com>
2022-08-29 12:06:53 +0800

    重构

--- app/models/validation_code.rb
+++ app/models/validation_code.rb
@@ -1,4 +1,16 @@
 class ValidationCode < ApplicationRecord
     #email 必填
     validates :email, presence: true
+
+    before_create :generate_code
+    after_create :send_email
+  
+    enum kind: { sign_in: 0, reset_password: 1 }
+  
+    def 
+      self.code = SecureRandom.random_number.to_s[2..7]
+    end
+    def send_email
+      UserMailer.welcome_email(self.email)
+    end
 end
