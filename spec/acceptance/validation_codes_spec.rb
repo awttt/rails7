bbdf9bcfa6bac8b7c3872ea9c4a155b953557786
awt <350944364@qq.com>
2022-08-30 17:38:02 +0800

    配置文件修改

--- spec/acceptance/validation_codes_spec.rb
+++ spec/acceptance/validation_codes_spec.rb
@@ -3,11 +3,8 @@ require 'rspec_api_documentation/dsl'
 
 resource "验证码" do
   post "/api/v1/validation_codes" do
-
     parameter :email, type: :string
-    header "Content-Type", "application/json"
     let(:email) {'1@qq.com'}
-
     example "请求发送验证码" do
       expect(UserMailer).to receive(:welcome_email).with(email)
       do_request
