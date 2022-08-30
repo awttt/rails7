2768ace3b15ace467d8b3e82ed274ef440c59bb4
awt <350944364@qq.com>
2022-08-30 23:43:28 +0800

    fix

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,23 +1,14 @@
-require "jwt"
-
+require 'jwt'
 class Api::V1::SessionsController < ApplicationController
-  def create
-    #如果是测试环境 code 就永远为 123456
+  def create 
     if Rails.env.test?
-      return render status: :unauthorized if params[:code] != "123456"
+      return render status: :unauthorized if params[:code] != '123456'
     else
-      canSignin = ValidationCode.exists? email: params[:email], code: params[:code], used_at: nil
+      canSignin = ValidationCodes.exists? email: params[:email], code: params[:code], used_at: nil
       return render status: :unauthorized unless canSignin
     end
-    user = User.find_by_email params[:email]
-    if user.nil?
-      render status: 404, json: { errors: "用户不存在" }
-    else
-      payload = { user_id: user.id }
-      token = JWT.encode payload, Rails.application.credentials.hmac_secret, "HS256"
-      render status: 200, json: { jwt: token }
-    end
+    user = User.find_or_create_by email: params[:email]
+    render status: :ok, json: { jwt: user.generate_jwt }
+
   end
 end
-
-# 查看 email code 且used_at=nil 的记录
