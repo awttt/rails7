66b198ec04c85cdba8d5224ebb3c69099a15fdd7
awt <350944364@qq.com>
2022-09-01 22:29:24 +0800

    items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -12,11 +12,12 @@ class Api::V1::ItemsController < ApplicationController
     }}
   end
   def create
-    item = Item.new amount: params[:amount]
+    item = Item.new params.permit(:amount, :happen_at, tags_id: [] )  
+    item.user_id = request.env['current_user_id']
     if item.save
       render json: {resource: item}
     else
-      render json: {errors: item.errors}, status: 422
+      render json: {errors: item.errors}, status: :unprocessable_entity
     end
   end
 end
