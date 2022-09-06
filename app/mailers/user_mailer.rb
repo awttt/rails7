a7b7f644508a23c9e9de5bb2666d2942f979b595
awt <350944364@qq.com>
2022-09-06 23:22:42 +0800

    fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,8 @@
 class UserMailer < ApplicationMailer
   def welcome_email(email)
-    #.order(create_at: :desc) 找最后生成的数据
-    validation_code = ValidationCode.order(create_at: :desc).find_by_email(email)
+    validation_code = ValidationCode.order(created_at: :desc).find_by_email(email)
     @code = validation_code.code
-    mail(to: email, subject: '验证码')
+    mail(to: email, subject: '山竹记账验证码')
   end
 end
  
