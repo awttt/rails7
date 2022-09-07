95b63cabb5bcab91b791b36315259d55a7a4b0f5
awt <350944364@qq.com>
2022-09-07 22:40:54 +0800

    国际化 & 邮件格式校验

--- app/models/validation_code.rb
+++ app/models/validation_code.rb
@@ -1,17 +1,17 @@
 class ValidationCode < ApplicationRecord
-    #email 必填
-    validates :email, presence: true
-    
-    before_create :generate_code
-    after_create :send_email
-    
-    #枚举
-    enum kind: { sign_in: 0, reset_password: 1 }
+  validates :email, presence: true
+  # email 必须是合法的邮箱地址
+  validates :email, format: {with: /\A.+@.+\z/}
   
-    def generate_code
-      self.code = SecureRandom.random_number.to_s[2..7]
-    end
-    def send_email
-      UserMailer.welcome_email(self.email).deliver
-    end
+  before_create :generate_code
+  after_create :send_email
+
+  enum kind: { sign_in: 0, reset_password: 1 }
+
+  def generate_code
+    self.code = SecureRandom.random_number.to_s[2..7]
   end
+  def send_email
+    UserMailer.welcome_email(self.email)&.deliver
+  end
+end
